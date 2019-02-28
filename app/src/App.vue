2be0781d390680a1a1bc7a89bce6fa7ab9c348dd
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2019-02-28 22:31:18 +0000

    Rendering rich text

--- app/src/App.vue
+++ app/src/App.vue
@@ -16,7 +16,6 @@ html, body {
   font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 #nav {
--- app/src/components/TextField.js
+++ app/src/components/TextField.js
@@ -5,7 +5,61 @@ export default {
   },
   render: function(h) {
     const richText = (type, obj) => {
-      return h(type, obj.text);
+      const n = obj.spans ? obj.spans.length : 0;
+      let spans = [];
+      for(let i = 0; i < n; ++i) {
+        const s = obj.spans[i];
+        spans.push({ type: s.type, index: s.start, end: false });
+        spans.push({ type: s.type, index: s.end, end: true });
+      }
+      spans.sort((a, b) => {
+        if(a.index != b.index) {
+          return a.index - b.index;
+        }
+        return a.end - b.end;
+      });
+
+      let types = {};
+      let ranges = [];
+
+      for(let i = 0; i < spans.length-1; ++i) {
+        const s = spans[i];
+        const t = spans[i+1];
+        let n = s.index;
+        if(s.end) {
+          delete types[s.type];
+          ++n;
+        } else {
+          types[s.type] = true;
+        }
+        let m = t.index;
+        if(!t.end) {
+          --m;
+        }
+
+        if(n < m) {
+          ranges.push({types: Object.keys(types), start: n, end: m});
+        }
+      }
+
+      let rendered = [];
+      let j = 0;
+      ranges.forEach(r => {
+        if(j < r.start) {
+          rendered.push(obj.text.substring(j, r.start));
+        }
+        let m = h(r.types[0], obj.text.substring(r.start, r.end + 1));
+        for(let i = 1; i < r.types.length; ++i) {
+          m = h(r.types[i], [m]);
+        }
+        rendered.push(m);
+        j = r.end + 1;
+      });
+      if(j < obj.text.length) {
+        rendered.push(obj.text.substring(j));
+      }
+
+      return h(type, rendered);
     };
     const image = (type, obj) => {
       return h(type, {attrs: {src: obj.url, alt: obj.alt}});
