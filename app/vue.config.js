63fb9395ab95f0dab60e436255f75d3c857f5fce
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2019-03-03 23:13:34 +0000

    Adds the theme color to the vue config

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? 'https://static.leftoverbytes.com/'
-    : '/'
+    : '/',
+
+  pwa: {
+    name: 'Leftover Bytes',
+    themeColor: '#af4212',
+  }
 }
