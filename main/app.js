fdfbeb0f33639f993abee879f80fdfe5e7097730
Ryan Calhoun <magnet-dev+rc@asynchrony.com>
2019-03-05 08:32:07 -0600

    Refactors sitemap creation using xmlbuilder

--- main/app.js
+++ main/app.js
@@ -2,68 +2,16 @@
 
 const express = require('express');
 const fs = require('fs');
-const prismic = require('prismic-javascript');
+const sitemap = require('./sitemap');
 
 const app = express();
 
 app.get('/sitemap.xml', (req, res) => {
-  const elem = (e) => {
-    res.write('<' + e.tag);
-
-    if(e.attrs) {
-      Object.keys(e.attrs).forEach((key) => {
-        const val = e.attrs[key];
-        res.write(' ' + key + '="' + val + '"');
-      });
-    }
-    res.write('>');
-
-    if(Array.isArray(e.children)) {
-      res.write('\n');
-      e.children.forEach((c) => {
-        elem(c);
-      });
-    } else if(typeof e.children == "string"){
-      res.write(e.children);
-    }
-
-    res.write('</' + e.tag + '>\n');
-  }
-
+  res.setHeader("Content-Type", "application/xml");
   res.writeHead(200);
-
-  const schema = {
-    'xmlns:xsi' : 'http://www.w3.org/2001/XMLSchema-instance',
-    'xmlns': 'http://www.sitemaps.org/schemas/sitemap/0.9',
-    'xsi:schemaLocation': 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd',
-  };
-
-  const baseurl = 'https://leftoverbytes.cdn.prismic.io/api/v2';
-  prismic.getApi(baseurl).then((api) => {
-    api.query("").then((docs) => {
-
-      const children = [];
-      docs.results.forEach((post) => {
-        let path = '/';
-        if(post.type == 'post') {
-          const date = new Date(post.first_publication_date);
-          path = '/posts/' + date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + post.uid + '/'
-        } else if(post.type == 'info') {
-          path = '/' + post.uid + '/';
-        } else if(post.type == 'author'){
-          path = '/authors/' + post.uid + '/';
-        }
-        children.push({
-          tag: 'url', children: [
-            {tag: 'loc', children: 'https://leftoverbytes.com' + path},
-            {tag: 'lastmod', children: post.last_publication_date.replace("+0000", "+00:00")},
-          ]
-        });
-      });
-
-      elem({tag: 'urlset', attrs: schema, children: children});
-      res.end();
-    });
+  sitemap.generate().then((xml) => {
+    res.write(xml);
+    res.end();
   });
 });
 
---
+++ main/sitemap.js
@@ -0,0 +1,36 @@
+const builder = require('xmlbuilder');
+const prismic = require('prismic-javascript');
+
+class Sitemap {
+  constructor() {
+    const baseurl = 'https://leftoverbytes.cdn.prismic.io/api/v2';
+    this.api = prismic.getApi(baseurl);
+  }
+
+  async generate() {
+    const doc = builder.create('urlset');
+    doc.att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+    doc.att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+    doc.att('xsi:schemaLocation', 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd');
+
+    (await (await this.api).query("")).results.forEach((p) => {
+      let path = '/';
+      if(p.type == 'post') {
+        const date = new Date(p.first_publication_date);
+        path = '/posts/' + date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + p.uid + '/'
+      } else if(p.type == 'info') {
+        path = '/' + p.uid + '/';
+      } else if(p.type == 'author'){
+        path = '/authors/' + p.uid + '/';
+      }
+
+      doc.ele('url')
+        .ele('loc').txt('https://leftoverbytes.com' + path).up()
+        .ele('lastmod').txt(p.last_publication_date.replace("+0000", "+00:00")).up()
+      .up();
+    });
+
+    return doc.toString({pretty: true});
+  }
+}
+module.exports = new Sitemap();
