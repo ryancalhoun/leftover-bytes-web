3d909f2b9f71b9934e2bd524290b73f7cbcf4c03
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2019-04-27 15:24:50 +0000

    Include embed.ly in valid hostnames

--- main/medium-import.js
+++ main/medium-import.js
@@ -81,7 +81,7 @@ class MediumImport {
     const clientIp = (this.req.header('X-Forwarded-For') || '').split(',').shift();
     return new Promise((resolve, reject) => {
       dns.reverse(clientIp, (e, hostnames) => {
-        if(hostnames.filter(h => h.match(/amazonaws.com/)).length > 0) {
+        if(hostnames.filter(h => h.match(/embed.ly|amazonaws.com/)).length > 0) {
           this.getDocument().then((page) => {
             this.render(res, page);
             resolve();
