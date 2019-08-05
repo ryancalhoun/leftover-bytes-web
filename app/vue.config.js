99f5fd9d9b4ef0542088c55f9f5915f97f6d0488
Ryan Calhoun <magnet-dev+rc@asynchrony.com>
2019-08-05 17:57:44 -0500

    Use the plain google url instead of the load balancer

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://static.leftoverbytes.com/'
+    ? 'https://storage.googleapis.com/leftoverbytes-content/'
     : '/',
 
   pwa: {
--- main/prep-assets.sh
+++ main/prep-assets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sed -i 's|rel=manifest .*manifest.json|rel=manifest href=/manifest.json|' index.html
-sed -i 's|"./img|"https://static.leftoverbytes.com/img|' manifest.json
+sed -i 's|"./img|"https://storage.googleapis.com/leftoverbytes-content/img|' manifest.json
