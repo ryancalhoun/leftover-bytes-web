075714151e2550b7469c7e3072574f817952e49a
Ryan Calhoun <ryan.calhoun@wwt.com>
2019-11-21 10:17:02 -0600

    Only does GA in prod

--- app/src/google/tracking.js
+++ app/src/google/tracking.js
@@ -1,17 +1,21 @@
 class Tracking {
   attach() {
-    let ga = document.createElement('script');
-    ga.async = true;    
-    ga.src = "https://www.googletagmanager.com/gtag/js?id=UA-89944251-4";
-    document.head.appendChild(ga);
+    if(process.env.NODE_ENV === 'production') {
+      let ga = document.createElement('script');
+      ga.async = true;    
+      ga.src = "https://www.googletagmanager.com/gtag/js?id=UA-89944251-4";
+      document.head.appendChild(ga);
 
-    window.dataLayer = window.dataLayer || [];
-    window.gtag = function(){window.dataLayer.push(arguments);}
-    window.gtag('js', new Date());
-    window.gtag('config', 'UA-89944251-4');
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function(){window.dataLayer.push(arguments);}
+      window.gtag('js', new Date());
+      window.gtag('config', 'UA-89944251-4');
+    }
   }
   update(path) {
-    window.gtag('config', 'UA-89944251-4', {page_path: path});
+    if(process.env.NODE_ENV === 'production') {
+      window.gtag('config', 'UA-89944251-4', {page_path: path});
+    }
   }
 }
 
--- app/src/main.js
+++ app/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import adsense from './google/adsense'
 import tracking from './google/tracking'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
