240db9a7fafd2249b673d7b389bdee0ce51aec6a
Ryan Calhoun <ryan.calhoun@wwt.com>
2019-11-26 12:37:53 -0600

    WIP the oauth code exchange

--- app/vue.config.js
+++ app/vue.config.js
@@ -16,21 +16,6 @@ module.exports = {
       },
       '/oauth.*': {
         target: 'https://20191126t172616-dot-leftoverbytes.appspot.com/',
-        onProxyRes: res => {
-/*
-          const loc = new URL(res.headers['location']);
-          const redirect = new URL(loc.searchParams.get('redirect_uri'));
-          console.log(loc);
-          console.log(redirect);
-          redirect.protocol = 'http:';
-          redirect.hostname = 'localhost';
-          console.log(redirect);
-    
-          loc.searchParams.set('redirect_uri', redirect.toString());
-          console.log(loc);
-          res.headers['location'] = loc.toString();
-*/
-        },
       },
     }
   }
--- main/oauth.js
+++ main/oauth.js
@@ -1,13 +1,10 @@
 const Router = require('express').Router();
 const qs = require('querystring');
+const https = require('https');
+const jwtDecode = require('jwt-decode');
 
 Router.get('/', async (req, res) => {
 
-});
-
-Router.get('/cookie', (req, res) => {
-  res.cookie('test', 'value');
-  res.end();
 });
 
 Router.get('/google', (req, res) => {
@@ -21,7 +18,7 @@ Router.get('/google', (req, res) => {
     response_type: 'code',
     redirect_uri: redirectUrl.toString(),
     scope: 'openid profile email',
-    state: returnUrl,
+    state: returnUrl.toString(),
     prompt: 'select_account',
   };
 
@@ -29,7 +26,7 @@ Router.get('/google', (req, res) => {
   res.end();
 });
 
-Router.get('/google/verify', async (req, res) => {
+Router.get('/google/verify', (req, res) => {
   const code = req.query.code;
   const returnUrl = new URL(req.query.state);
 
@@ -37,13 +34,41 @@ Router.get('/google/verify', async (req, res) => {
     const redirectUrl = new URL(returnUrl.toString());
     redirectUrl.pathname = req.path;
 
-    const opts = {
+    const payload = {
       client_id: '403632071908-7v9k2mk0cdbqpg698hd1rsklt86rd4k8.apps.googleusercontent.com',
       client_secret: 'NffUEV1tF66e79vPrR9aPt3F',
       code: code,
-      redirect_uri: redirectUrl,
+      redirect_uri: redirectUrl.toString(),
       grant_type: 'authorization_code',
     };
+
+    const opts = {
+      hostname: 'oauth2.googleapis.com',
+      port: 443,
+      path: '/token',
+      method: 'POST',
+    };
+
+    console.log(payload);
+
+    const onExchange = (data) => {
+      const userData = jwtDecode(data.id_token);
+      console.log(userData);
+
+      res.send(JSON.stringify(userData));
+      res.end();
+    };
+
+    const exchange = https.request(opts, res = > {
+      let body = '';
+      res.on('data', (chunk) => body += chunk);
+      res.on('end', () => {
+        console.log(body);
+        onExchange(JSON.parse(body));
+      });
+    });
+    exchange.write(querystring.stringify(payload));
+    exchange.end();
   } else {
 
   }
