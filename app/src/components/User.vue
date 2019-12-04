061d633414aaacedaacfe8d4c2d212b50c4cf809
Ryan Calhoun <ryan.calhoun@wwt.com>
2019-12-04 14:58:46 -0600

    Forgot to add the user component

---
+++ app/src/components/User.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="user" v-if="user">
+    <img v-bind:src="user.picture"/>
+    <span> {{ user.name }} </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['user']
+}
+</script>
+
+<style scoped lang="scss">
+.user {
+  padding: 8px 0;
+  img, span {
+    display: inline-block;
+    vertical-align: bottom;
+    height: 24px;
+  }
+  img {
+    border-radius: 50%;
+    width: 24px;
+    margin: 0;
+  }
+  span {
+    padding: 0 8px;
+    line-height: 24px;
+    color: #999;
+  }
+}
+</style>
